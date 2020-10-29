d0def8bd2949e553f41ddf6220c6aa843c691b19
muhodev <hi@muho.dev>
2020-10-29 11:45:07 +0300

    basic styling Link component

--- components/Link/index.js
+++ components/Link/index.js
@@ -1,9 +1,25 @@
 import NextLink from "next/link"
 import styled from "styled-components"
+import {
+    space,
+    color,
+    fontSize,
+    fontWeight
+} from "styled-system"
 
 const StyledLink = styled.a`
-    color: currentColor;
+    text-decoration: none;
+    display: inline-block;
+    &:hover { text-decoration: underline; }
+    ${space}
+    ${color}
+    ${fontSize}
+    ${fontWeight}
 `
+
+StyledLink.defaultProps = {
+    color: "primaryColor"
+}
 
 function Link({ href, children }) {
     return (
