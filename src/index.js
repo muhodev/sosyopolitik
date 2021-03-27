624011db6f1850adf085681b0bc1e27f73d8a097
muhodev <hi@muho.dev>
2021-03-27 16:48:29 +0300

    installed tailwind and cofigured it

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 
+import "style/index.css";
+
 import { App } from "app";
 
 ReactDOM.render(<App />, document.getElementById("root"));
