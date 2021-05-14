a2ce3dcbf6b8556a9d2d793e9019fd410d83edb7
muho <hi@muho.dev>
2021-05-14 13:52:01 +0300

    created signup page

--- formSchemas/login.js
+++ formSchemas/login.js
@@ -10,3 +10,16 @@ export const loginSchema = yup.object().shape({
     .min(5, 'Şifre alanı en az 5 karakter içermelidir.')
     .required('Şifre alanı zorunludur.')
 });
+
+export const signupSchema = yup.object().shape({
+  displayName: yup.string().required('İsim alanı zorunludur'),
+  email: yup
+    .string()
+    .required('Email alanı zorunludur')
+    .email('Lütfen geçerli bir email adresi girin.'),
+  username: yup.string().required('Kulanıcı adı alanı zorunludur'),
+  password: yup
+    .string()
+    .min(5, 'Şifre alanı en az 5 karakter içermelidir.')
+    .required('Şifre alanı zorunludur.')
+});
