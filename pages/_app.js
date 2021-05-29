f759bbc71f6aae842c08cdb9b8b783284f19b65c
muho <hi@muho.dev>
2021-05-29 13:02:32 +0300

    nprogress eklendi

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,18 @@
 import { useEffect, useRef } from 'react';
+import Router from 'next/router';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Hydrate } from 'react-query/hydration';
+import NProgress from 'nprogress'; //nprogress module
+
 import { AuthProvider } from 'providers';
 import { AuthConsumer } from 'consumers';
+
+import 'nprogress/nprogress.css'; //styles of nprogress
 import 'style/index.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   const queryClientRef = useRef();
